c9cee8d5eed121ec82bb0dfcc59488ddccd4c292
Kadhar-learn <itsmekadhar@gmail.com>
2024-12-08 09:11:27 +0530

    Completed Week1.Day1 Assignments
    HelloTestLeaf.java
    DataTypes.java

---
+++ day1/DataTypes.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		float version = 2.5f;
+		String Group = "GEMINI";
+		Boolean isBeta = false;
+		int CurrentYear = 2024;
+		char shortcutkey = 'a';
+		
+		System.out.println("Float value entered is "+version);
+		System.out.println("Group Name: "+Group);
+		System.out.println("Current Year: "+CurrentYear);
+		System.out.println("Shortcut key is: "+shortcutkey);
+		System.out.println("Boolean Output:" +isBeta);
+	}
+
+}
